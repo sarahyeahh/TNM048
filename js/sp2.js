4acb392ae41d3b891dfaff345aefdca34a36bdd3
sarahyeahh <sarah_f_92@hotmail.com>
2017-03-07 14:02:29 +0100

    ny sp

--- js/sp2.js
+++ js/sp2.js
@@ -1,17 +1,17 @@
 //Måste ha d3 v4 för att fungera!
 
-function sp(){
+function sp2(){
 
   var self = this;
 
-    var spDiv = $("#sp");
+    var spDiv = $("#sp2");
 
     var margin = {top: 20, right: 20, bottom: 30, left: 90},
         width = spDiv.width() - margin.right - margin.left,
         height = spDiv.height() - margin.top - margin.bottom;
      
     var varXaxis = "";
-    var varYaxis = "";
+    var varYaxis = "";        
 
     var color = d3.scaleOrdinal(d3.schemeCategory20); //new v4 
 
@@ -26,11 +26,8 @@ function sp(){
     var xAxis = d3.axisBottom(x).tickFormat(d3.format("d")); 
     var yAxis = d3.axisLeft(y);
 
-    var brush = d3.brush().extent([[0, 0], [width, height]])
-                .on("end", brushended), idleTimeout, idleDelay = 350;
-
-    var svg = d3.select("#sp").append("svg")
-              .attr("id", "g1_svg")
+    var svg = d3.select("#sp2").append("svg")
+              .attr("id", "g2_svg")
               .attr("data-margin-right", margin.right)
               .attr("data-margin-left", margin.left)
               .attr("data-margin-top", margin.top)
@@ -53,24 +50,30 @@ function sp(){
              .attr("clip-path", "url(#clip)");
 
     //Load data
-    d3.csv("data/movies.csv", function(error, data) {
+    d3.csv("data/movie_metadata.csv", function(error, data) {
         self.data = data;
     
         //define the domain of the scatter plot axes
-        varXaxis = "year";
-        varYaxis = "domgross";
-      //  max = 3000000000; 
-        max = 800000000;
+        varXaxis = "imdb_score";
+        varYaxis = "gross";
 
-        x.domain(d3.extent(data, function(d){return d[varXaxis];}));
+        max =  800000000;
+
+       // x.domain(d3.extent(data, function(d){return d[varXaxis];}));
+        x.domain([0, 10]); 
         y.domain([0, max]); 
 
         draw();
-        countPass();
 
     });
 
     function draw(){
+
+        var cc = {};
+
+        self.data.forEach( function(d){
+            cc[d.movie_title] = color(d.movie_title);
+        } );
 
         svg.append("g")
          .attr("class", "x axis ")
@@ -106,22 +109,16 @@ function sp(){
             .attr("class", "dot")
             //Define the x and y coordinate data values for the dots
             .attr("r", 3)
-            .attr("cx", function(d) {return x(d[varXaxis]);})
-            .attr("cy", function(d) { 
-                if (d[varYaxis] == "#N/A")
-                {
-                    //Om något värde är #N/A ska den sättas till -500000.
-                    d[varYaxis]=-5000000;
-                } 
-                return y(d[varYaxis]);
-            })
-            .style("fill", function(d) { if (d.binary != "PASS"){ return "red"} else{return "green"}})
+            .attr("cx", function(d) { return x(d[varXaxis]);})
+            .attr("cy", function(d) {return y(d[varYaxis]);})
+            //.style("fill", function(d) { if (d.binary != "PASS"){ return "red"} else{return "green"}})
+            .style("fill", function(d) {return cc[d.movie_title]})
             //tooltip
             .on("mousemove", function(d) {
                 tooltip.transition()
                    .duration(20)
                    .style("opacity", 0.9);
-                tooltip.html(d.title + "<br/> Income: "  + d[varYaxis] + "<br/> Year: " + d[varXaxis])
+                tooltip.html(d.movie_title + "<br/> Income: "  + d[varYaxis] + "<br/> imdbRating: " + d[varXaxis])
                    .style("left", (d3.event.pageX + 5) + "px")
                    .style("top", (d3.event.pageY - 28) + "px");   
                 })
@@ -130,84 +127,15 @@ function sp(){
             })
             .on("click",  function(d) {
                 var array = [];
-                array[d.title] = d.title;
+                array[d.movie_title] = d.movie_title;               
+                window.open(d.movie_imdb_link);
               //  sp1.selectDot(array);
-              //  pc1.selectLine(array);  
+              //  pc1.selectLine(array);
+                
             });
 
-        scatter.append("g")
-            .attr("class", "brush")
-            .call(brush);
-    
     }
 
-
-    function brushended() {
-
-            var s = d3.event.selection;
-
-            if (!s) {
-                if (!idleTimeout) return idleTimeout = setTimeout(idled, idleDelay);
-                  x.domain(d3.extent(self.data, function(d){return d[varXaxis];}));
-                  y.domain([0, max]); 
-            } else {
-                
-                x.domain([s[0][0], s[1][0]].map(x.invert, x));
-                y.domain([s[1][1], s[0][1]].map(y.invert, y));
-      
-                scatter.select(".brush")
-                    .call(brush.move, null);
-            }
-            zoom();
-    }
-
-    function idled() {
-            idleTimeout = null;
-    }
-
-    function zoom() {
-
-        var t = scatter.transition().duration(750);
-        svg.select("#axis--x").transition(t).call(xAxis);
-        svg.select("#axis--y").transition(t).call(yAxis);
-
-        scatter.selectAll("circle").transition(t)
-            .attr("cx", function (d) { return x(d[varXaxis]); })
-            .attr("cy", function (d) { return y(d[varYaxis]); });
-    }
-
-    //Count all the movies whether they fail or pass. 
-    function countPass(){
-
-        var pass = 0; 
-        var fail = 0; 
-        var all = 0; 
-
-        self.data.forEach( function(d){
-             
-             if(d.binary== "PASS"){  
-                pass++; 
-             }
-             else{
-                fail++; 
-             }
-
-            all++;
-
-         } );
-
-        //Writes in <div id="count"> how many movies that pass/fail.
-       document.getElementById("count").innerHTML = "<b>Pass:</b> " + pass + "<br> <b>Fail:</b> " + fail + "<br> of total:" + all + " movies."; 
-
-    }
-
-    // function passfail(){
-    //        // console.log(d.binary);
-
-    //         var dots = d3.selectAll(".dot")
-    //            .style("fill", function(d) { console.log(d.binary); if (d.binary != "PASS"){ return "red"} else{return "green"}})
-
-    // }
 
    // // //method for selecting the dot from other components
    //  this.selectDot = function(value){
@@ -221,4 +149,6 @@ function sp(){
    //  function selFeature(value){
    //      //...
    //  }
+
+
 }
